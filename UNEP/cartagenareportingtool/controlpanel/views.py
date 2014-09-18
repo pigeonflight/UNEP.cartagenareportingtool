760b2f506d556165e961962e09ea18940997f181
David Bain <david@alteroo.com>
2014-09-18 10:12:20 -0500

    updated stylesheets and data for controlpanel

--- UNEP/cartagenareportingtool/controlpanel/views.py
+++ UNEP/cartagenareportingtool/controlpanel/views.py
@@ -3,6 +3,8 @@
 from five import grok
 #from UNEP.cartagenareportingtool.interface import IReportStatus
 from Products.CMFCore.interfaces import ISiteRoot
+from plone import api
+
 
 #from plone.z3cform import layout
 #from plone.directives import form, dexterity
@@ -23,12 +25,53 @@ class ControlPanelView(grok.View):
     grok.name("cartagenareportingtool-controlpanel")
     grok.context(ISiteRoot)
 
+    def get_country_report(self,user):
+        pm = api.portal.get_tool(name='portal_membership')
+        members_folder = pm.membersfolder_id
+        path = "/%s/%s/country-report" % (members_folder,user)
+        
+        try:
+            country_report = api.content.get(path=path)
+        except AttributeError:
+            country_report = None
+        
+        return country_report
+
     def status_info(self):
-        return [
-              {'name':"Jamaica",
-               'status':"good",
-                'progress':10},
-               {'name':"Trinidad",
-               'status':"also good",
-               'progress':50}
-              ]
+        """
+        returns data in the form
+          [
+                {'username':"JAM",
+                'name':"Jamaica",
+                 'status':"good",
+                  'progress':10},
+                 {'username':'TNT',
+                 name':"Trinidad",
+                 'status':"also good",
+                 'progress':50}
+                ]
+        """
+        user_status = []
+        users = api.user.get_users()
+        for user in users:
+            username = user.id
+            country_report = self.get_country_report(username)
+            progress = 0
+            doclink = ''
+            status = 'not started'
+            css_class = "%s %s" % (username,'not-started')
+            if country_report:
+                progress = 5
+                doclink = country_report.absolute_url()
+                status = 'started'
+                css_class = "%s %s" % (username,'started')
+            props = {}
+            props['username'] = username
+            props['name'] = user.getProperty('fullname')
+            props['status'] = status
+            props['class']  = css_class
+            props['progress'] = progress
+            props['doclink']  = doclink
+            user_status.append(props)
+        return user_status
+        
--- UNEP/cartagenareportingtool/setuphandlers.py
+++ UNEP/cartagenareportingtool/setuphandlers.py
@@ -5,7 +5,10 @@ from Products.CMFPlone.interfaces import constrains
 from plone.app.controlpanel.security import ISecuritySchema
 import logging
 from zExceptions import BadRequest
+import Globals
 
+
+logger = logging.getLogger(__name__)
 
 PROFILE_ID='profile-UNEP.cartagenareportingtool:default'
 PRODUCT = 'UNEP.cartagenareportingtool'
@@ -18,16 +21,20 @@ def setupVarious(context):
     
     if api.content.get('/news'):
         api.content.delete(api.content.get('/news'))
+        logger.info('removed the news folder')
     if api.content.get('/Members'):
         api.content.delete(api.content.get('/Members'))
+        logger.info('removed the Members folder')
     if api.content.get('/events'):   
         api.content.delete(api.content.get('/events'))
+        logger.info('removed the events folder')
 
     portal = api.portal.get()
 
     # configure default site view to be 'site_index'
     portal.manage_changeProperties(default_page='site_index')
     portal.setLayout("site_index")
+    logger.info('replaced the default front page of the site')
     # we will be organizing folders by year
     # starting by hardcoding for 2014
     # no longer using the reports folder 
@@ -42,7 +49,9 @@ def setupVarious(context):
             id=memberfolder,
             title='%s report' % memberfolder
             )
+            logger.info('created custom memberfolder called %s' % memberfolder)
         except BadRequest:
+            logger.info('unable to create custom memberfolder called %s' % memberfolder)
             return
         api.content.transition(reportfolder, transition='publish')
         behavior = constrains.ISelectableConstrainTypes(reportfolder)
@@ -53,12 +62,14 @@ def setupVarious(context):
     # set type to custom member type
     mp.setMemberAreaType('UNEP.cartagenareportingtool.memberfolder')
     # set member folder name
-    print "setting members folder"
+    
     mp.setMembersFolderById(memberfolder)
+    logger.info("set members folder to %s" % memberfolder)
     
     # call update security 
     set_up_security(context)
     set_up_users(context)
+    logger.info("created memberstate user accounts")
 
 def set_up_users(context):
     memberstates = context.readDataFile('memberstates.csv')
@@ -73,11 +84,22 @@ def set_up_users(context):
             )
         username = member[0]
         email = member[3]
-        user = api.user.create(
-                  username=username,
-                  email=email,
-                  properties=properties,
-                )
+        if '@' not in email:
+            email = "changeme@example.com"
+        print "using: ",email
+        if Globals.DevelopmentMode:
+            user = api.user.create(
+                      username=username,
+                      email=email,
+                      password='password',
+                      properties=properties,
+                    )
+        else:
+            user = api.user.create(
+                      username=username,
+                      email=email,
+                      properties=properties,
+                    )
 
 def set_up_security(context):
     """ Enable/disable security controlpanel (a.k.a. @@security-controlpanel)
